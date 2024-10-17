1e2cf873e74645d7ac5b600172de1afcab88e8cb
hseun <hseun0732@dms.hs.kr>
2024-10-17 19:20:18 +0900

    style :: color

--- app/src/main/java/com/second/connect/ui/theme/Color.kt
+++ app/src/main/java/com/second/connect/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.second.connect.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Main1 = Color(0xFFFFAFBD)
+val Main2 = Color(0xFFFFC3A0)
+val Main = Brush.verticalGradient(listOf(Main1, Main2))
+
+val Gray100 = Color(0xFFEBEBEB)
+val Gray200 = Color(0xFFD3D3D3)
+val Gray300 = Color(0xFFBEBEBE)
+val Gray400 = Color(0xFFA7A7A7)
+val Gray500 = Color(0xFF929292)
+val Gray600 = Color(0xFF7C7C7C)
+val Gray700 = Color(0xFF505050)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFEA2E33)
+val DialogBlue = Color(0xFF5EB3F7)
