ea979a01c9e312a57c0879929ff32a022866eb3c
hseun <hseun0732@dms.hs.kr>
2024-10-17 19:20:04 +0900

    style :: font

--- app/src/main/java/com/second/connect/ui/theme/Type.kt
+++ app/src/main/java/com/second/connect/ui/theme/Type.kt
@@ -1,10 +1,13 @@
+
 package com.second.connect.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.second.connect.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +34,12 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_black, FontWeight.Black)
 )
