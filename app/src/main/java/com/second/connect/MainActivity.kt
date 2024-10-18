6d46a6973a4ca2fa08b94a42e24382ea896134a2
hseun <hseun0732@dms.hs.kr>
2024-10-18 16:15:46 +0900

    ✨ :: Navigation

--- app/src/main/java/com/second/connect/MainActivity.kt
+++ app/src/main/java/com/second/connect/MainActivity.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.second.connect.ui.login.LoginScreen
 import com.second.connect.ui.theme.ConnectTheme
@@ -41,7 +54,6 @@ fun ConnectApp(modifier: Modifier = Modifier) {
                 }
             )
         }
-
         composable("main") {
             ConnectScreen(
                 navController = navController
@@ -53,6 +65,13 @@ fun ConnectApp(modifier: Modifier = Modifier) {
 @Composable
 fun ConnectScreen(
     modifier: Modifier = Modifier,
-    navController: NavController
+    navController: NavHostController
 ) {
+    Scaffold(
+        bottomBar = { ConnectBottomNavigation(navController = navController) }
+    ) {
+        Box(modifier.padding(it)) {
+            ConnectGraph(navController = navController)
+        }
+    }
 }
