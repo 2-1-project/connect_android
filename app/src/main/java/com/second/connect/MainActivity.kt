2223d24f5a1e8b54161fb1fb9cdb753bdfd39bc2
hseun <hseun0732@dms.hs.kr>
2024-10-18 11:24:42 +0900

    🌼 :: NavController

--- app/src/main/java/com/second/connect/MainActivity.kt
+++ app/src/main/java/com/second/connect/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.second.connect.ui.login.LoginScreen
 import com.second.connect.ui.theme.ConnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConnectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ConnectApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ConnectApp(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onLoginButtonClick = {
+                    // 로그인
+                    navController.navigate("main")
+                }
+            )
+        }
+
+        composable("main") {
+            ConnectScreen(
+                navController = navController
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ConnectTheme {
-        Greeting("Android")
-    }
+fun ConnectScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
 }
